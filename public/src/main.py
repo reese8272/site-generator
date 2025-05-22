c4928d68f3169e50eca71490671bb9a6d236165e
reese8272 <reesepludwick@gmail.com>
2025-05-22 00:33:59 -0400

    added images and index for future generation. Created a file copying function and added an extract title function for future

--- public/src/main.py
+++ public/src/main.py
@@ -1,9 +1,29 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from markdown_blocks import markdown_to_blocks
+from copystatic import copy_files_recursive
 
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def extract_title(markdown):
+    blocks = markdown_to_blocks(markdown)
+    block = blocks[0]
+    if block.startswith("#") and block[1:2] != "#":
+        title = block[1:].strip()
+        return title
+    else:
+        raise Exception("No Heading Found")
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 main()
