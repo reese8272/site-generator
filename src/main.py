e67a0d47f559359f7b6a78efe2173e9fa446b409
reese8272 <reesepludwick@gmail.com>
2025-05-23 22:55:51 -0400

    final touches, build.sh and docs file for the full website structure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
+import sys
 import shutil
 from markdown_blocks import markdown_to_blocks, markdown_to_html_node
 from copystatic import copy_files_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 
 
 def extract_title(markdown):
@@ -18,7 +19,7 @@ def extract_title(markdown):
         raise Exception("No Heading Found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as f:
         markdown = f.read()
@@ -29,33 +30,39 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     final_html = template.replace("{{ Title }}", title)
     final_html = final_html.replace("{{ Content }}", content)
+    final_html = final_html.replace('href="/', 'href="' + basepath)
+    final_html = final_html.replace('src="/', 'src="' + basepath)
     directory_path = os.path.dirname(dest_path)
     os.makedirs(directory_path, exist_ok = True)
     with open(dest_path, 'w') as f:
         f.write(final_html)
     
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for file in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, file)
         if os.path.isfile(entry_path):
             if entry_path.endswith(".md"):
-                generate_page(entry_path, template_path, os.path.join(dest_dir_path, file.replace(".md",".html")))
+                generate_page(entry_path, template_path, os.path.join(dest_dir_path, file.replace(".md",".html")), basepath)
         elif os.path.isdir(entry_path):
             dest_path = os.path.join(dest_dir_path, file)
             if not os.path.exists(dest_path):
                 os.makedirs(dest_path)
-            generate_pages_recursive(entry_path, template_path, dest_path)
+            generate_pages_recursive(entry_path, template_path, dest_path, basepath)
         else:
             raise Exception("Invalid format for generation.")
 
 def main():
-    print("Deleting public directory...")
+    if len(sys.argv) == 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
-    generate_pages_recursive("./content/", "./template.html", "./public")
+    generate_pages_recursive("./content/", "./template.html", dir_path_public, basepath)
 
 
 main()
